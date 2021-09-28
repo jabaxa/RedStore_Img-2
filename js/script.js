c54fc5c93e1f5696481d256fe5180cebf5799cf2
JonSnow <jabaxa1995@gmail.com>
2021-09-28 15:59:03 +0500

    28.09.2021 16:00

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ MenuItems.style.maxHeight = "0px";
 function menutoggle() {
 	if (MenuItems.style.maxHeight == "0px") {
 		MenuItems.style.maxHeight = "200px";
-	};
+	}
 	else {
 		MenuItems.style.maxHeight = "0px";
-	};
+	}
 };
 
 menutoggle();
